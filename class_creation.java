3bb5829c6f2f17e5286f051d47575db68a91058a
cxsper <ycasperinho@gmail.com>
2020-06-05 10:30:00 +0300

    Update class_creation.java

--- class_creation.java
+++ class_creation.java
@@ -1,32 +1,46 @@
+/**
+* Class creation practice
+* 
+* (c) Yaroslav Kasperovhych
+* MIT License
+* /
+
 package com.company;
 
 import java.util.Objects;
 
+// Class Person
 public class Person {
     int age;
     String name;
 
+    // common constructor
     public Person(String name, int age) {
         this.age = age;
         this.name = name;
     }
 
+    // method of speaking 
     public void say(String what) {
         System.out.println(what);
     }
     
+    // method of barking 
     public void bark() {
         System.out.println("woof woof");
     }
     
+    // method of dancing 
     public void dance() {
         System.out.println("I am dancing");
     }
     
+    // method of calculating area of a rectangle 
     public double calculateAreaOfARectangle(double a, double b) {
         return a * b;
     }
     
+    // method of 2+2 sum
     public int superComplicatedMethod() {
         return 2 + 2;
     }
