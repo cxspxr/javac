6729eeac000e56ea74399893339af1414eb788de
cxsper <ycasperinho@gmail.com>
2020-06-08 23:04:04 +0300

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -7,7 +7,71 @@
 
 package com.company;
 
-// Person class was in class_creation.java task, this is just continuation of that task
+public class Person {
+    int age;
+    String name;
+    // common constructor
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+    // method of speaking 
+    public void say(String what) {
+        System.out.println(what);
+    }
+    
+    // method of barking 
+    public void bark() {
+        System.out.println("woof woof");
+    }
+    
+    // method of dancing 
+    public void dance() {
+        System.out.println("I am dancing");
+    }
+    
+    // method of calculating area of a rectangle 
+    public double calculateAreaOfARectangle(double a, double b) {
+        return a * b;
+    }
+    
+    // method of 2+2 sum
+    public int superComplicatedMethod() {
+        return 2 + 2;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+}
+
 public class Kasperovych extends Person {
     public Kasperovych(String name, int age) {
         super(name, age);
